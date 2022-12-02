fbfafce5359fe972b628e6938c69b17e951f47f9
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2022-12-03 00:17:54 +0100

    updates + styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 import { useLanguage } from './context/LanguageContext';
-import Test from './components/Test';
+import Navbar from './components/navbar/Navbar';
+import Hero from './components/hero/Hero';
 
 const App = () => {
   const { language } = useLanguage();
 
   return (
-    <div dir={language === 'arabic' ? 'rtl' : 'ltr'}>
-      <Test />
+    <div className='app' dir={language === 'arabic' ? 'rtl' : 'ltr'}>
+      <nav className='app-header'>
+        <Navbar />
+      </nav>
+      <header>
+        <Hero />
+      </header>
     </div>
   );
 };
