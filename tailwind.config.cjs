1cc83f465f634a20e3d4a6ad3d5f1e42b9c1a2f0
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2022-12-03 00:18:03 +0100

    updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        paytoneOne: ['"Paytone One"', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
