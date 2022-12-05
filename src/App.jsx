986f43420240ed8017183373df437bf4ed7e05e6
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2022-12-05 14:33:39 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,34 @@ import './App.css';
 import { useLanguage } from './context/LanguageContext';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
+import Announcements from './components/announcements/Announcements';
+import Nursersies from './components/nurseries/Nursersies';
+import Jobs from './components/jobs/Jobs';
+import Store from './components/store/Store';
 
 const App = () => {
   const { language } = useLanguage();
 
   return (
     <div className='app' dir={language === 'arabic' ? 'rtl' : 'ltr'}>
-      <nav className='app-header'>
+      <div className='app-header'>
         <Navbar />
-      </nav>
+      </div>
       <header>
         <Hero />
       </header>
+      <section>
+        <Announcements />
+      </section>
+      <section>
+        <Nursersies />
+      </section>
+      <section>
+        <Jobs />
+      </section>
+      <section>
+        <Store />
+      </section>
     </div>
   );
 };
