739bd7f3c70cafea933c54275806ff7d0e5e136d
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2022-12-12 18:14:11 +0100

    update hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,29 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLanguage } from '../../context/LanguageContext';
 import img from '../../assets/img/hero_image_01.png';
 import img2 from '../../assets/img/hero_rotated_image_01.png';
-import location from '../../assets/img/location.png';
-import location2 from '../../assets/img/location2.png';
+import { useNavigate } from 'react-router-dom';
+import Model from '../model/Model';
 
 const Hero = () => {
+  const [open, setOpen] = useState(false);
   const { language, data } = useLanguage();
+
+  const handleOpen = () => setOpen(!open);
+
   return (
     <div className={`hero ${language === 'english' ? 'pl-10' : 'pr-10'}`}>
       <div className='left_hero'>
         <h1>
           <span>{data?.hero.title1}</span> <br /> {data?.hero.title2}
         </h1>
-        <div className='flex items-center'>
-          <img
-            src={language === 'english' ? location : location2}
-            alt=''
-            className='w-[100px]'
-          />
-          <div className='hero_location'>
-            <h4>{data?.hero.location}</h4>
-            <span>{data?.hero.adress}</span>
-          </div>
-        </div>
+        <button
+          onClick={handleOpen}
+          className='bg-[#37AC94] px-4 py-2 rounded-full rounded-tl-none text-white lg:flex lg:items-center lg:gap-2 animation mt-6 text-lg font-normal mb-10'>
+          {data?.login?.button?.title}
+        </button>
+        <Model
+          data={data?.users}
+          handleLanguageChange={null}
+          open={open}
+          handleOpen={handleOpen}
+        />
       </div>
       <div className=''>
         <img src={language === 'english' ? img : img2} alt='hero imgae' />
