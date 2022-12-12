81bcf5b0aad3f4ce161a9755e6aeb2adf6024222
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2022-12-12 18:14:39 +0100

    make model reuseable

--- src/components/model/Model.jsx
+++ src/components/model/Model.jsx
@@ -1,73 +1,45 @@
 import { Fragment, useState } from 'react';
 import {
-  Button,
   Dialog,
   DialogHeader,
   DialogBody,
-  DialogFooter,
   Radio,
+  Button,
 } from '@material-tailwind/react';
-import { SiGoogletranslate } from 'react-icons/si';
-import { useLanguage, useLanguageUpdate } from '../../context/LanguageContext';
+import { useLanguage } from '../../context/LanguageContext';
+import { useNavigate } from 'react-router-dom';
 
-const Model = () => {
-  const { language, data } = useLanguage();
-  const toggleLanguage = useLanguageUpdate();
-  const [open, setOpen] = useState(false);
-  const [selectedValue, setSelectedValue] = useState(data?.tr?.languages[0]);
-
-  const handleOpen = () => setOpen(!open);
-
-  const handleLanguageChange = (e) => {
-    console.log(e);
-    setSelectedValue(() => e.value);
-    console.log(selectedValue);
-    toggleLanguage(e);
-    handleOpen();
-  };
-
+const Model = ({ data, handleLanguageChange, open, handleOpen, field }) => {
+  const { language } = useLanguage();
+  const navigate = useNavigate();
   return (
     <Fragment>
-      <Button
-        onClick={handleOpen}
-        variant='text'
-        className='flex gap-2 items-center justify-center bg-transparent text-black border-collapse hover:bg-[#37AC94] hover:bg-opacity-20'>
-        {language === 'english' ? 'En' : 'Ar'} | <SiGoogletranslate />
-      </Button>
-      <Dialog open={open} handler={handleOpen}>
-        <DialogHeader>{data?.tr?.title}</DialogHeader>
+      <Dialog
+        open={open}
+        handler={handleOpen}
+        dir={language === 'english' ? 'ltr' : 'rtl'}
+        className='w-1/4'>
+        <DialogHeader>{data?.title}</DialogHeader>
         <DialogBody divider>
-          <div className='flex gap-10'>
-            {data &&
-              data?.tr?.languages.map((lang) => (
-                <>
-                  <Radio
-                    key={Math.floor(Math.random() * 100000)}
-                    id={`${lang.label}`}
-                    name='language'
-                    label={`${lang.title}`}
-                    // ripple={true}
-                    onChange={() => setSelectedValue(lang.label)}
-                  />
-                </>
-              ))}
+          <div className='flex flex-col gap-2'>
+            {data?.items.map((item, i) => (
+              <div key={i}>
+                <Radio
+                  type='radio'
+                  id={item.label}
+                  name={field}
+                  label={item.title}
+                  onChange={() =>
+                    field === 'language'
+                      ? handleLanguageChange(item.label)
+                      : navigate(`/register${item.label}`)
+                  }
+                  checked={language === item.label ? true : false}
+                />
+              </div>
+            ))}
           </div>
         </DialogBody>
-        <DialogFooter>
-          <Button
-            variant='text'
-            color='red'
-            onClick={handleOpen}
-            className='mr-1'>
-            <span>{data?.tr?.cancel}</span>
-          </Button>
-          <Button
-            variant='gradient'
-            color='green'
-            onClick={() => handleLanguageChange(selectedValue)}>
-            <span>{data?.tr?.confirm}</span>
-          </Button>
-        </DialogFooter>
       </Dialog>
     </Fragment>
   );
