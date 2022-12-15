34dbf55cb75c73626f93e0f5ae1d2748b8958db6
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2022-12-15 17:27:17 +0100

    add general landing page (re-structuring the code)

---
+++ src/pages/landing/LandingPage.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Hero from '../../components/hero/Hero';
+import Announcements from '../../components/announcements/Announcements';
+import Section from '../../components/section/Section';
+import Login from '../../components/login/Login';
+
+const LandingPage = ({ data }) => {
+  return (
+    <>
+      <header>
+        <Hero />
+      </header>
+      <main>
+        <section>
+          <Announcements />
+        </section>
+        {/* nurseries */}
+        <Section
+          data={data?.nurseries}
+          imgFirst={true}
+          sectionTitle='nurseries'
+        />
+        {/* jobs */}
+        <Section data={data?.jobs} sectionTitle='jobs' />
+        {/* store */}
+        <Section data={data?.store} imgFirst={true} sectionTitle='store' />
+        <Section data={data?.consultation} sectionTitle='consultation' />
+        <Login data={data?.login} usersData={data?.users} />
+      </main>
+    </>
+  );
+};
+
+export default LandingPage;
