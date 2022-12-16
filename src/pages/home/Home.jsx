ab390d83c6c3f52c2cc3059e53aca7dc61ad438e
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2022-12-16 13:54:44 +0100

    add home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Posts from '../../components/posts/Posts';
+import Sidebar from '../../components/sidebar/Sidebar';
+import img from '../../assets/img/bottom_white_wave_01.png';
+import { useLanguage } from '../../context/LanguageContext';
+
+const Home = () => {
+  const { language } = useLanguage();
+  return (
+    <div>
+      <div className='flex flex-col lg:flex-row mt-16'>
+        <main className='w-full lg:max-w-[80%]'>
+          <Posts />
+        </main>
+        <aside className='w-full lg:max-w-[25%] flex flex-col lg:flex-row mt-16'>
+          <Sidebar />
+        </aside>
+      </div>
+      <div className='bg-[#FAF5F2] mt-4'>
+        <img src={img} alt='' />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
